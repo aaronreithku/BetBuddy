b488f25b48b14623a5de1b6b1b1bfcf67014d440
Traizen <traizenmckown@gmail.com>
2022-11-15 17:04:55 -0600

    Fixed Text and made it to where buttons are not on top of each other

--- app/src/main/java/com/example/betbuddy/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/betbuddy/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        //mText.setValue("Welcome to BetBuddy");
     }
 
     public LiveData<String> getText() {
