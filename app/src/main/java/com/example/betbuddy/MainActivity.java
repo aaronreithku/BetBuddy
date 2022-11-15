71bafff537211c86dffd20dae98ab403a57a8ced
dmitay <49884602+dmitay@users.noreply.github.com>
2022-11-15 16:39:28 -0600

    First commit, added track history page, and button should now link to that page

--- app/src/main/java/com/example/betbuddy/MainActivity.java
+++ app/src/main/java/com/example/betbuddy/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.betbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +18,7 @@ import com.example.betbuddy.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Button track_history_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        track_history_button = (Button) findViewById(R.id.button);
+        track_history_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTrackHistory();
+            }
+        });
+
+        }
+
+    public void openTrackHistory() {
+        Intent intent = new Intent(this, Track_History.class);
+        startActivity(intent);
     }
 
 }
