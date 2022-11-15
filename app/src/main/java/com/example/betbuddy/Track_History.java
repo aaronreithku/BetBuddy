e0cf79c808311831b1f71e4cb4632e547e35b195
Aaron Reith <aaronreith@ku.edu>
2022-11-15 17:43:49 -0600

    Linked buttons

--- app/src/main/java/com/example/betbuddy/Track_History.java
+++ app/src/main/java/com/example/betbuddy/Track_History.java
@@ -2,13 +2,31 @@ package com.example.betbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Track_History extends AppCompatActivity {
+
+    private Button add_New_Bet_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_history);
+        add_New_Bet_button = (Button) findViewById(R.id.button);
+        add_New_Bet_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddBet();
+            }
+        });
+
+    }
+
+    public void openAddBet() {
+        Intent intent = new Intent(this, fragment_addBet.class);
+        startActivity(intent);
     }
 }
