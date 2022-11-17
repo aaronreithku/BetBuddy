15177e1b2c99dc9c14e9623908ffebaa419594c6
Aaron Reith <aaronreith@ku.edu>
2022-11-17 14:59:48 -0600

    Changed UI of add bet and updated gradle

--- app/src/main/java/com/example/betbuddy/add_bet.java
+++ app/src/main/java/com/example/betbuddy/add_bet.java
@@ -10,11 +10,23 @@ import android.widget.Button;
 public class add_bet extends AppCompatActivity {
 
     private Button submit_button;
+    private Button spread_button;
+    private Button ML_button;
+    private Button overUnder_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_bet);
+        spread_button = (Button) findViewById(R.id.betSpread);
+        ML_button = (Button) findViewById(R.id.betMoneyline);
+        overUnder_button = (Button) findViewById(R.id.OverUnder);
+        spread_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setPresetOdds();
+            }
+        });
         submit_button = (Button) findViewById(R.id.button);
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +38,9 @@ public class add_bet extends AppCompatActivity {
     public void openTrackHistory() {
         Intent intent = new Intent(this, Track_History.class);
         startActivity(intent);
+    }
+
+    public void setPresetOdds() {
+
     }
 }
